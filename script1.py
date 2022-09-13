f90b402478ad600c0c9311e8a662ca85505ee77e
Taranjot Singh <taranjotec@gmail.com>
2022-09-12 18:48:42 -0600

    finalize script for lab1

--- script1.py
+++ script1.py
@@ -5,4 +5,4 @@ print(requests.__version__)
 response = requests.get(
     "https://raw.githubusercontent.com/taranjot-s/cmput-404-labs/main/script1.py")
 
-print(response.content)
+print(response.text)
